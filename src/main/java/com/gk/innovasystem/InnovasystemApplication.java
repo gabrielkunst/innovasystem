18539c4c349a2f60ec9a652c117f80fb5f6cea3d
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-09-15 19:44:00 -0300

    chore: add project setup

---
+++ src/main/java/com/gk/innovasystem/InnovasystemApplication.java
@@ -0,0 +1,13 @@
+package com.gk.innovasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovasystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnovasystemApplication.class, args);
+    }
+
+}
